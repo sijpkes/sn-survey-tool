efc263841efef490719c0bac7268a385d688d9b1
Paul Sijpkes <paul@sijpkes.net>
2013-10-09 11:39:25 +1100

    script now more self-contained as a plug-in

--- js/core.js
+++ js/core.js
@@ -1,7 +1,20 @@
-$(document).ready(function() {
-	
-		var urls = ["/ps_survey/form1.html", "/ps_survey/form2.html"];
+(function ( $ ) {
+    
+    var iframe = "<iframe src=''></iframe>";
+      
+    $.fn.addSurvey = function(params) {
+		var container = this;
 		
+		if(typeof params.base === 'undefined') {
+		    params.base = "/";
+		}
+		if(typeof params.guidSearchTerm === 'undefined') {
+		    params.guidSearchTerm = "%for_script_use%";   
+		}
+		console.log(params);
+		var n = 0;
+        var loads = 0;
+        
 		var gen_id = function() {
 			function S4() {
     			return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
@@ -11,29 +24,33 @@ $(document).ready(function() {
 		};
 		
 		var guid = gen_id(); 
-				
-		$("#frame-content iframe").attr("src", urls[0]);
 		
-		var n = 0;
-		var loads = 0;
+		var addFrame = function() {
+		    console.log(params);
+		    $(container).prepend(iframe);
+		    $("iframe", container).first().attr("src", params.base+params.urls[n++]);
+		    
+		    $("iframe", container).first().on('load', function() { 
+            //++loads;
+            console.log("loaded iframe "+loads);
+        
+            if(++loads % 2 === 0) {
+                        $("iframe", container).first().hide();
+                        addFrame();
+                        return false;
+            } 
+            
+            var contents = $("iframe", container).first().contents();
+            var uid_store = $(".ss-q-title:contains('"+params.guidSearchTerm+"')", contents);
+            
+            var input = $(uid_store).parent().parent().find("input");
+            $(input).val(guid);
+            $(uid_store).parent().parent().hide();
+        });
+		};
 		
-		$('#frame-content iframe').on('load', function() {	
-			++loads;
-			console.log("loaded iframe "+loads);
 		
-			if(loads == 2) {
-						$("#frame-content iframe").attr("src", urls[++n]);
-						return false;
-			} 
-			
-			var contents = $(this).contents();
-			var uid_store = $(".ss-q-title:contains('%for_script_use%')", contents);
-			
-			var input = $(uid_store).parent().parent().find("input");
-			$(input).val(guid);
-			$(uid_store).parent().parent().hide();
-		});
+		addFrame();		
+};
 		
-		
-		
-	});
+}( jQuery ));
