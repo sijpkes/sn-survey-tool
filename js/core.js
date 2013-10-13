355faddc9ecab81bd7838b235911ee2f0299b04e
Paul Sijpkes <paul@sijpkes.net>
2013-10-13 21:28:49 +1100

    Update

--- js/core.js
+++ js/core.js
@@ -1,9 +1,12 @@
 (function ( $ ) {
     
-    var iframe = "<iframe src=''></iframe>";
+    var iframe = "<iframe src='' seamless></iframe>";
       
     $.fn.addSurvey = function(params) {
-		var container = this;
+        var list = $(this).find("div:eq(0)");
+		var container = $(this).find("div:eq(1)");
+		var lockFrame = false;
+		var userName = "";
 		
 		if(typeof params.base === 'undefined') {
 		    params.base = "/";
@@ -36,12 +39,18 @@
         
             if(++loads % 2 === 0) {
                         $("iframe", container).first().hide();
+                        lockFrame = false;
                         addFrame();
                         return false;
-            } 
+            } else {
+                lockFrame = true;
+            }
             
             var contents = $("iframe", container).first().contents();
             var uid_store = $(".ss-q-title:contains('"+params.guidSearchTerm+"')", contents);
+            var name = $("div.ss-form-desc:contains('%user_name%')", contents);
+            var rtext = $(name).html().replace('%user_name%', userName);
+            $(name).html(rtext);    
             
             var input = $(uid_store).parent().parent().find("input");
             $(input).val(guid);
@@ -49,8 +58,30 @@
         });
 		};
 		
+		var ticks = function() {
+		    $("label", list).prepend("<input type='radio' name='radio' value='1'/>");
+		    
+		    $(list).on('click', 'label input', function() {
+		       if(lockFrame) return false; 
+		       
+		       if($(this).attr('type') == 'radio') {
+		          
+		          userName = $($(this).closest('label')).text();
+		         
+		          $(this).closest('ul').prev("p").text("Thank you, "+userName+". Please fill in your details below."); 
+		          $(this).closest('ul').hide().attr('id','name_list');
+		          $(this).closest('li').remove();
+		          $('input', list).attr('disabled', 'disabled');
+		          lockFrame = true;
+		          addFrame();
+		       } else if($(this).attr('type') == 'checkbox') {
+		           $("input",this).attr("disabled", "disabled ");
+                   addFrame();
+		       }
+		    });
+		};
 		
-		addFrame();		
+		ticks();		
 };
 		
 }( jQuery ));
