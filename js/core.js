1dbd631b44c6d15f379789a821cf283dc59adc35
Paul Sijpkes <paul@sijpkes.net>
2013-10-14 21:53:09 +1100

    added skip function

--- js/core.js
+++ js/core.js
@@ -29,17 +29,26 @@
 		var guid = gen_id(); 
 		
 		var addFrame = function() {
-		    console.log(params);
+		   if(typeof params.frames[n].message === 'undefined') {
+		      params.frames[n].message = "";    
+		   }
+		   if(typeof params.frames[n].anonymous === 'undefined') {
+              params.frames[n].anonymous = true;    
+           }
+		   
 		    $(container).prepend(iframe);
-		    $("iframe", container).first().attr("src", params.base+params.urls[n++]);
+		    $("iframe", container).first().attr("src", params.base+params.frames[n].url);
 		    
 		    $("iframe", container).first().on('load', function() { 
-            //++loads;
-            console.log("loaded iframe "+loads);
-        
+          
             if(++loads % 2 === 0) {
                         $("iframe", container).first().hide();
                         lockFrame = false;
+                        
+                        if(n >= params.frames.length) {
+                                n = 0;
+                        }
+                        n = n + 1;
                         addFrame();
                         return false;
             } else {
@@ -48,18 +57,46 @@
             
             var contents = $("iframe", container).first().contents();
             var uid_store = $(".ss-q-title:contains('"+params.guidSearchTerm+"')", contents);
-            var name = $("div.ss-form-desc:contains('%user_name%')", contents);
-            var rtext = $(name).html().replace('%user_name%', userName);
-            $(name).html(rtext);    
             
-            var input = $(uid_store).parent().parent().find("input");
-            $(input).val(guid);
+            if(!params.frames[n].anonymous) {
+                var input = $(uid_store).parent().parent().find("input");
+                $(input).val(guid);
+            }
+            if(!params.frames[n].userFocused) {
+                var nextName = $("label.waiting", list).first();
+                $(nextName).removeClass('waiting').addClass('current');
+                var screen_name = $(nextName).text();
+                
+                $(".ss-form-entry:contains('%screen_name%'), .ss-form-desc:contains('%screen_name%')", contents).each(function() {
+                       var myContents = $(this).html();
+                       $(this).html(myContents.replace('%screen_name%', screen_name)); 
+                });
+            }
+            if(params.frames[n].message.length > 0) {
+                $("p", list).first().html(params.frames[n].messsage);
+            }
+            if(params.frames[n].skipButton) {
+               if($("#skip", list).length === 0) { 
+                    $(list).append("<button id='skip'>Skip this person</button>");
+                    $(list).on('click', '#skip', function() {
+                            $("label.waiting", list).removeClass('waiting').addClass('skipped');
+                            if(n >= params.frames.length) {
+                                n = 0;
+                            }
+                            n = n + 1;
+                            addFrame();
+                    });
+               }
+            }
+            
+            console.log(params);
+            
             $(uid_store).parent().parent().hide();
-        });
+            });
 		};
 		
 		var ticks = function() {
-		    $("label", list).prepend("<input type='radio' name='radio' value='1'/>");
+		    $("label", list).prepend("<input type='radio' name='radio' value='1'/>").addClass('waiting');
 		    
 		    $(list).on('click', 'label input', function() {
 		       if(lockFrame) return false; 
@@ -68,16 +105,13 @@
 		          
 		          userName = $($(this).closest('label')).text();
 		         
-		          $(this).closest('ul').prev("p").text("Thank you, "+userName+". Please fill in your details below."); 
+		          $(this).closest('ul').prev("p").html("Thank you, "+userName+". Please fill in your details below."); 
 		          $(this).closest('ul').hide().attr('id','name_list');
 		          $(this).closest('li').remove();
 		          $('input', list).attr('disabled', 'disabled');
 		          lockFrame = true;
 		          addFrame();
-		       } else if($(this).attr('type') == 'checkbox') {
-		           $("input",this).attr("disabled", "disabled ");
-                   addFrame();
-		       }
+		       } 
 		    });
 		};
 		
